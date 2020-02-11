ca00e5cfc585a6f39dfeacecb86f25f9e5280c7c
robschroeder333 <theguyonthecouch7@gmail.com>
2020-02-11 15:28:07 -0800

    squash sizing bugs

--- TheSyndicate/ConsoleWindow.cs
+++ TheSyndicate/ConsoleWindow.cs
@@ -6,8 +6,8 @@ namespace TheSyndicate
     public static class ConsoleWindow
     {
 
-		public static int Width = 200;
-		public static int Height = 300;
+		public static int Width = 160;
+		public static int Height = 50;
 
         [DllImport("libc")]
         private static extern int system(string exec);
@@ -16,7 +16,8 @@ namespace TheSyndicate
         {
             if (Program.isWindows)
             {
-                Console.SetBufferSize(Width-1, Height-1);
+                Console.SetWindowSize(1, 1);
+                Console.SetBufferSize(Width+2, Height+2);
                 Console.SetWindowSize(Width, Height);
             }
             else
